f7a1be5946f949d4523f2563ceddd3f2235b47d7
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-07 02:55:55 +0300

    Add basic argspase functional (args)

--- difference_finder/scripts/gendiff.py
+++ difference_finder/scripts/gendiff.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', metavar='first_file')
+    parser.add_argument('second_file', metavar='second_file')
+    
+    args = parser.parse_args()
+    first_file_name = args.first_file
+    second_file_name = args.second_file
+
+
+if __name__ == '__main__':
+    main()
