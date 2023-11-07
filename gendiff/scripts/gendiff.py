5beef7407e0f35698d712dcd18c65596e70c50e7
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-07 14:20:52 +0300

    Add --format arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,11 +10,11 @@ def main():
     )
     parser.add_argument('first_file', metavar='first_file')
     parser.add_argument('second_file', metavar='second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     
     args = parser.parse_args()
     first_file_name = args.first_file
     second_file_name = args.second_file
-
-
+    
 if __name__ == '__main__':
     main()
