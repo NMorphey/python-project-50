56ede87016230793e54dead1d6bd1f56cd769a6c
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-08 00:10:14 +0300

    Add test

--- tests/test_json.py
+++ tests/test_json.py
@@ -6,5 +6,14 @@ FILE_2_JSON_NAME = 'file2.json'
 
 
 def test_example_files_comparision():
-    pass
+    assert generate_diff(FILE_1_JSON_NAME, FILE_2_JSON_NAME) == (
+        """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
+    )
     
