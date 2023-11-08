869d76b43155c703f9757c67f6748a781e0fe593
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-09 02:40:39 +0300

    Fix

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -2,8 +2,10 @@ from gendiff import generate_diff
 
 
 def test_example_files():
-    json_1 = 'tests/fixtures/flat_json/example_files_1.json'
-    json_2 = 'tests/fixtures/flat_json/example_files_2.json'
-    result = 'tests/fixtures/flat_json/example_files_result'
-    assert generate_diff(json_1, json_2) == result
+    json_1_path = 'tests/fixtures/flat_json/example_files_1.json'
+    json_2_path = 'tests/fixtures/flat_json/example_files_2.json'
+    result_path = 'tests/fixtures/flat_json/example_files_result'
+    with open(result_path) as file:
+      result = file.read()
+    assert generate_diff(json_1_path, json_2_path) == result
     
