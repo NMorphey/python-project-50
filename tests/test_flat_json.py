bf9a57a24e8b9dec9c653a6fa55259eb220c8c9b
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-08 23:41:57 +0300

    Update paths, move them to the test function

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -1,19 +1,9 @@
 from gendiff import generate_diff
 
 
-FILE_1_JSON_PATH = 'tests/file1.json'
-FILE_2_JSON_PATH = 'tests/file2.json'
-
-
-def test_example_files_comparision():
-    assert generate_diff(FILE_1_JSON_PATH, FILE_2_JSON_PATH) == (
-        """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
-    )
+def test_example_files():
+    json_1 = 'tests/fixtures/flat_json/example_files_1.json'
+    json_2 = 'tests/fixtures/flat_json/example_files_2.json'
+    result = 'tests/fixtures/flat_json/example_files_result'
+    assert generate_diff(json_1, json_2) == result
     
