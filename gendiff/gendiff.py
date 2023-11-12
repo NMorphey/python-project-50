b5b994f8c2304d79711e0bdf710b9e4a89e9cbed
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-13 02:42:10 +0300

    Apply linter fixes

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,4 +1,4 @@
-from json import loads, dumps
+from json import loads
 
 
 IDENTS = {
@@ -22,29 +22,29 @@ def generate_diff(file_path_1, file_path_2) -> str:
         json_2 = loads(file_2.read(), parse_constant=True)
     all_keys = set(json_1.keys()) | set(json_2.keys())
     all_keys_sorted = sorted(list(all_keys))
-    
+
     changes = []
     for key in all_keys_sorted:
         key_in_json_1 = key in json_1
         key_in_json_2 = key in json_2
         if key_in_json_1 and not key_in_json_2:
-            changes.append( ('removed', key, json_1[key]) )
+            changes.append(('removed', key, json_1[key]))
         elif key_in_json_2 and not key_in_json_1:
-            changes.append( ('added', key, json_2[key]) )
+            changes.append(('added', key, json_2[key]))
         elif key_in_json_1 and key_in_json_2:
             json_1_value = json_1[key]
             json_2_value = json_2[key]
             if json_1_value == json_2_value:
-                changes.append( ('remained', key, json_1_value) )
+                changes.append(('remained', key, json_1_value))
             else:
-                changes.append( ('removed', key, json_1_value) )
-                changes.append( ('added', key, json_2_value) )
-    
+                changes.append(('removed', key, json_1_value))
+                changes.append(('added', key, json_2_value))
+
     result = '{\n'
     for ident, key, value in changes:
         if value in INCORRENTLY_PARSABLE_CONSTANTS:
             value = INCORRENTLY_PARSABLE_CONSTANTS[value]
         result += f'{IDENTS[ident]}{key}: {value}\n'
     result += '}'
-    
+
     return result
