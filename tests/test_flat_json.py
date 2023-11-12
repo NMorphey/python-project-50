591d6f5864dceb3b110f1ecb6cff4aaa1b6b5b99
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-13 02:39:19 +0300

    Add tests

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -8,4 +8,25 @@ def test_example_files():
     with open(result_path) as file:
       result = file.read()
     assert generate_diff(json_1_path, json_2_path) == result
-    
+
+def test_empty():
+    json_path = 'tests/fixtures/flat_json/empty.json'
+    result_path = 'tests/fixtures/flat_json/empty_result'
+    with open(result_path) as file:
+        result = file.read()
+    assert generate_diff(json_path, json_path) == result
+
+def test_similar():
+    json_path = 'tests/fixtures/flat_json/similar.json'
+    result_path = 'tests/fixtures/flat_json/similar_result'
+    with open(result_path) as file:
+        result = file.read()
+    assert generate_diff(json_path, json_path) == result
+
+def test_empty_and_filled():
+    json_1_path = 'tests/fixtures/flat_json/empty.json'
+    json_2_path = 'tests/fixtures/flat_json/similar.json'
+    result_path = 'tests/fixtures/flat_json/empty_and_filled_result'
+    with open(result_path) as file:
+      result = file.read()
+    assert generate_diff(json_1_path, json_2_path) == result
