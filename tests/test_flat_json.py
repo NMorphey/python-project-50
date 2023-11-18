f823197b601e9ce6ca105a16ea59682baf19e583
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-18 18:57:20 +0300

    Follow DRY

--- tests/test_flat_json.py
+++ tests/test_flat_json.py
@@ -1,32 +1,35 @@
 from gendiff import generate_diff
 
 
+FIXTURES_PATH = 'tests/fixtures/flat_json'
+
+
 def test_example_files():
-    json_1_path = 'tests/fixtures/flat_json/example_files_1.json'
-    json_2_path = 'tests/fixtures/flat_json/example_files_2.json'
-    result_path = 'tests/fixtures/flat_json/example_files_result'
+    json_1_path = f'{FIXTURES_PATH}/example_files_1.json'
+    json_2_path = f'{FIXTURES_PATH}/example_files_2.json'
+    result_path = f'{FIXTURES_PATH}/example_files_result'
     with open(result_path) as file:
       result = file.read()
     assert generate_diff(json_1_path, json_2_path) == result
 
 def test_empty():
-    json_path = 'tests/fixtures/flat_json/empty.json'
-    result_path = 'tests/fixtures/flat_json/empty_result'
+    json_path = f'{FIXTURES_PATH}/empty.json'
+    result_path = f'{FIXTURES_PATH}/empty_result'
     with open(result_path) as file:
         result = file.read()
     assert generate_diff(json_path, json_path) == result
 
 def test_similar():
-    json_path = 'tests/fixtures/flat_json/similar.json'
-    result_path = 'tests/fixtures/flat_json/similar_result'
+    json_path = f'{FIXTURES_PATH}/similar.json'
+    result_path = f'{FIXTURES_PATH}/similar_result'
     with open(result_path) as file:
         result = file.read()
     assert generate_diff(json_path, json_path) == result
 
 def test_empty_and_filled():
-    json_1_path = 'tests/fixtures/flat_json/empty.json'
-    json_2_path = 'tests/fixtures/flat_json/similar.json'
-    result_path = 'tests/fixtures/flat_json/empty_and_filled_result'
+    json_1_path = f'{FIXTURES_PATH}/empty.json'
+    json_2_path = f'{FIXTURES_PATH}/similar.json'
+    result_path = f'{FIXTURES_PATH}/empty_and_filled_result'
     with open(result_path) as file:
       result = file.read()
     assert generate_diff(json_1_path, json_2_path) == result
