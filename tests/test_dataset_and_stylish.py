bef6a0382b4280e768197258e7f9f35194e6158a
Ivlev Nikita <thel4stofme@yandex.ru>
2023-11-28 17:47:20 +0300

    Add test and fixture for stylish

---
+++ tests/test_dataset_and_stylish.py
@@ -0,0 +1,18 @@
+import pytest
+
+from gendiff.dataset import create_dataset
+from gendiff.stylish import stylish
+
+
+FIXTURES_PATH = 'tests/fixtures/stylish'
+
+
+def test_uncompared_datasets():
+    dataset_1 = create_dataset(
+        data={'key_1':
+        {'key_2': 'value_2',
+         'key_3': 'value_3'}}
+        )
+    with open(f'{FIXTURES_PATH}/result_1') as file:
+        result_1 = file.read()
+    assert stylish(dataset_1) == result_1
