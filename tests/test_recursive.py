422e3359a5c9904f7070448453856c6de490b220
Ivlev Nikita <thel4stofme@yandex.ru>
2023-12-02 05:12:41 +0300

    Fix tests

--- tests/test_recursive.py
+++ tests/test_recursive.py
@@ -1,3 +1,5 @@
+from json import loads
+
 from gendiff import generate_diff
 from gendiff.formaters.stylish import stylish
 from gendiff.formaters.plain import plain
@@ -28,6 +30,6 @@ def test_example_files():
     
     assert generate_diff(json_1_path, json_2_path, plain) == result_plain
     
-    with open(f'{FIXTURES_PATH}/example_result_plain') as file:
+    with open(f'{FIXTURES_PATH}/example_result.json') as file:
         result_json = file.read()
     assert generate_diff(json_1_path, json_2_path, json) == result_json
